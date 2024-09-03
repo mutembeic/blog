ee3f6ee23f852609d2853fd605351196ae8f0bc1
mutembeic <135004069+mutembeic@users.noreply.github.com>
2024-09-03 13:16:11 +0300

    json

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,10 @@
-import { Form, ToastContainer } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import { Button } from 'react-bootstrap';
 import '/home/charles/IST/phase2/projects/blog/src/index.css';
 import { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Create = () => {
 
@@ -18,11 +21,27 @@ const Create = () => {
             return {...prev, [name]: value}
         })
     }
-
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:4000/blog', data)
+        .then(res =>{
+            toast.success('new blog added successfully', {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 3000,
+            })
+        })
+        .catch(err =>{
+            toast.error('An error occurred while adding the blog', {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 3000,
+        })
+    })
+}
     return (
         <div className="content">
             <h2>Create a New Blog</h2>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="title">
                     <Form.Label>Title:</Form.Label>
                     <Form.Control name="title" type="text" placeholder="Enter the title" onChange={handleChange}/>
@@ -43,6 +62,8 @@ const Create = () => {
                 <ToastContainer />
 
                 <p>{data.title}</p>
+                <p>{data.author}</p>
+                <p>{data.body}</p>
             </Form>
         </div>
     );
