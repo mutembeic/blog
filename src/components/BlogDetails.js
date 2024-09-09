37376acaf9d014ec8e4000c53826241794236e24
mutembeic <135004069+mutembeic@users.noreply.github.com>
2024-09-09 12:43:41 +0300

    blog update

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,7 +1,37 @@
-const BlogDetails = () => {
-    return (
-        <h1>This is  BlogDetails</h1>
-    );
-}
+import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+import { Button } from "react-bootstrap";
+import {useHistory} from 'react-router-dom';
+import axios from 'axios';
 
+const BlogDetails = () => {
+    const {id} = useParams()
+    const {data:blogs} = useFetch('http://localhost:4000/blogs/' +id);
+    const history = useHistory();
+
+    const handleDelete = (e)=>{
+        e.preventDefault()
+
+        axios.delete('http://localhost:4000/blogs/' +id)
+        .then(res=>{
+            alert("blog deleted");
+            history.push('/');
+        })
+    }
+    return ( 
+        <div className="details">
+           
+            {blogs && (
+                <article>
+                    <h3>{blogs.title}</h3>
+                    <p> Done by: {blogs.author}</p>
+                    <div>{blogs.body}</div>
+                    <Button onClick={handleDelete} 
+                    variant="danger" className="mt-3" type="submit">delete Blog</Button>
+                </article>
+            )}
+        </div>
+     );
+}
+ 
 export default BlogDetails;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
-const Home = () => {
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () =>{
+    const {data: blogs} = useFetch('http://localhost:4000/blogs')
     return (
-        <h1>This is home</h1>
-    );
+        <div className="home">
+            {blogs && <BlogList blogs={blogs} title="All blogs" />}
+        </div>
+    )
 }
 
 export default Home;
